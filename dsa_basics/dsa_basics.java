04dac86ddb56a493b1697e9f02441201c4dc5343
ayush12993 <ayush2202ksr@gmail.com>
2024-11-05 16:56:59 +0530

    first commit

--- dsa_basics/dsa_basics.java
+++ dsa_basics/dsa_basics.java
@@ -3,3 +3,24 @@
 int fun1(int n){
        return n* (n+1)/2;
 }
+
+int fun2(int n){
+int sum =0;
+for(int i=0;i<=n;i++){
+ sum+=i;
+
+}
+return sum;
+
+}
+
+int fun3(int n){
+int sum = 0;
+for(int i=0;i<=n;i++){
+for(int j=1;j<=i;j++){
+sum++;
+}
+}
+return sum;
+
+}
