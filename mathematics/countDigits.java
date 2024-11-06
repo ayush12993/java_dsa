415297a9b1160add65f85eafe03239816c4704a6
ayush12993 <ayush2202ksr@gmail.com>
2024-11-06 16:32:51 +0530

    updated all mathematics programs

--- mathematics/countDigits.java
+++ mathematics/countDigits.java
@@ -1,8 +1,10 @@
-int countDigits(int x){
-int res=0;//setting result value to 0
-while(x>0){//iterating x till it is greater than 0
-x=x/10;//we divide until it gives 10
-res++;//we increment it by 1
-}
-return res;//then we will return the result
+class  countDigits{
+    int countDigits(int x){
+        int res=0;//setting result value to 0
+        while(x>0){//iterating x till it is greater than 0
+        x=x/10;//we divide until it gives 10
+        res++;//we increment it by 1
+        }
+        return res;//then we will return the result
+        }
 }
