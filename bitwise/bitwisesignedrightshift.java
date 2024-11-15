2572e030ca7ec5ba3dbd0088d222b1f1f15175fb
ayush12993 <ayush2202ksr@gmail.com>
2024-11-15 17:39:37 +0530

    adding signed right shift operator in java

---
+++ bitwise/bitwisesignedrightshift.java
@@ -0,0 +1,9 @@
+package bitwise;
+
+public class bitwisesignedrightshift {
+    public static void main(String[] args) {
+        int x=33;
+        System.out.println(x>>1);
+        System.out.println(x>>4);
+    }
+}
