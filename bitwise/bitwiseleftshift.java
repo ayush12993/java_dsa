4d1d8bda4a3caeb65e602676bf081d452719c341
ayush12993 <ayush2202ksr@gmail.com>
2024-11-15 16:55:18 +0530

    bitwise left implementation in java

---
+++ bitwise/bitwiseleftshift.java
@@ -0,0 +1,11 @@
+package bitwise;
+
+public class bitwiseleftshift {
+    public static void main(String[] args) {
+        int x=3;
+        System.out.println(x<<1);
+        System.out.println(x<<2);
+        int y=4;
+        System.out.println(x<<y);
+    }
+}
