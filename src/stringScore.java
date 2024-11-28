9784c5d157d3327cf7e91c21a279a8b5acd2ba66
ayush12993 <36598788+ayush12993@users.noreply.github.com>
2024-11-28 18:45:24 +0530

    This program is to ascii score of a string

--- src/stringScore.java
+++ src/stringScore.java
@@ -1,8 +1,21 @@
 public class stringScore {
     public static void main(String[] args) {
-        System.out.println(scoreOfString("hello"));
+        System.out.println(scoreOfString("zaz"));
     }
     public static int scoreOfString(String s) {
-       return 0;
+        char[] arrSpl = s.toCharArray();
+        int ans=0;
+        for (int i=1;i<arrSpl.length;i++){
+            char char_as_o = (char)arrSpl[i-1];
+            int ascii_one = char_as_o; char char_as_ao = (char)arrSpl[i];
+            int ascii_abone = char_as_ao;
+            if (ascii_one-ascii_abone<0){
+                ans+=(-(ascii_one-ascii_abone));
+            }else {
+                ans+=(ascii_one-ascii_abone);
+            }
+
+        }
+       return ans;
     }
 }
