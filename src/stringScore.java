712d0f5bf22485938f1ccee9030f33e786c9b63a
ayush12993 <36598788+ayush12993@users.noreply.github.com>
2024-11-28 18:35:23 +0530

    This program is to ascii score of a string

---
+++ src/stringScore.java
@@ -0,0 +1,8 @@
+public class stringScore {
+    public static void main(String[] args) {
+        System.out.println(scoreOfString("hello"));
+    }
+    public static int scoreOfString(String s) {
+       return 0;
+    }
+}
