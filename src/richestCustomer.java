b37ab81e63256598cf1c6cbcddf31c69bc7b88df
ayush12993 <36598788+ayush12993@users.noreply.github.com>
2024-12-01 16:35:17 +0530

    This program is to Find  Richest Customer Wealth adding optimized solution also

--- src/richestCustomer.java
+++ src/richestCustomer.java
@@ -1,6 +1,7 @@
 public class richestCustomer {
     public static void main(String[] args) {
         System.out.println(maximumWealth(new int[][] {{1,5},{3,7},{3,5}}));
+        System.out.println(optMaximumWealth(new int[][] {{1,5},{3,7},{3,5}}));
     }
     public static int maximumWealth(int[][] accounts) {
             int length = accounts.length;
@@ -25,5 +26,21 @@ public class richestCustomer {
 
             }
             return foundOne;
+    }
+    public static int optMaximumWealth(int[][] accounts) {
+        int length = accounts.length;
+        int tLength = accounts[0].length;
+        int calC=0,foundOne=0;
+        for (int i=0;i<length;i++){
+
+            calC=0;
+            for (int j=0;j<tLength;j++){
+
+                calC+=accounts[i][j];
+
+            }
+            foundOne=Math.max(calC,foundOne);
+        }
+        return foundOne;
     }
 }
