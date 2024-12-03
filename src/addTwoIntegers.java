b05e0f909adcfb6ce178a689096e66823276894f
ayush12993 <36598788+ayush12993@users.noreply.github.com>
2024-12-03 12:21:41 +0530

    This program is to  Add Two Integers

---
+++ src/addTwoIntegers.java
@@ -0,0 +1,8 @@
+public class addTwoIntegers {
+    public static void main(String[] args) {
+        System.out.println(sum(2,3));
+    }
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
